8fc4e3fd00f3d6eb80ce5e2aadaf8a677c4ffe38
dekunle02 <dekunle02@gmail.com>
2025-12-29 01:51:07 +0000

    torubleshooting backend access

--- homeserver-fe/src/utils.ts
+++ homeserver-fe/src/utils.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import type { AxiosInstance } from 'axios'
 
-const API_BASE_URL = 'http://127.0.0.1:8000/'
+// const API_BASE_URL = 'http://127.0.0.1:8000/'
+const API_BASE_URL = 'http://192.168.0.39:8000/'
 
 export const API_CLIENT: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
