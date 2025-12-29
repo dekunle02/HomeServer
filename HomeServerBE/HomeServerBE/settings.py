1a5158d9ffb0c3907d9ecb77372fdb730b9acd59
dekunle02 <dekunle02@gmail.com>
2025-12-29 01:45:13 +0000

    troubleshooting cors

--- HomeServerBE/HomeServerBE/settings.py
+++ HomeServerBE/HomeServerBE/settings.py
@@ -12,6 +12,26 @@ DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "192.168.0.39"]
 # CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://192.168.0.39:3000"]
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 # Application definition
 
